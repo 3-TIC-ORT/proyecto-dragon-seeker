dd481cbe0b0750fcd596ffd7a63a5c40b4d89cab
Lucas Waisbaum <you@example.com>
2025-08-13 10:38:42 -0300

    act

--- pelea.js
+++ pelea.js
@@ -1,3 +1,9 @@
-BotonPelea = document.getElementById(pelear);
-BotonAdopcion = document.getElementById(adoptar);
-MenuAtaques = document.getElementById(menudeataques);
+let BotonPelea = document.getElementById("pelear");
+let BotonAdopcion = document.getElementById("adoptar");
+let MenuAtaques = document.getElementById("menudeataques");
+function desplegarMenu(){
+    MenuAtaques.style.transform = "translateX(0%)";
+    BotonPelea.style.transform = "translateX(-200%)";
+    BotonAdopcion.style.transform = "translateX(-200%)";
+}
+BotonPelea.addEventListener("click", desplegarMenu);
