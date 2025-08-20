e2cb52416e5f4f8662efe51402cd5a9a4c61ac67
luquiwais <luquiwais@gmail.com>
2025-08-20 10:38:26 -0300

    arranca el codigo de la pelea

---
+++ pelea/ataques.js
@@ -0,0 +1,26 @@
+let ataque1 = document.getElementById("ataque1");
+let ataque2 = document.getElementById("ataque2");
+let ataque3 = document.getElementById("ataque3");
+let ataque4 = document.getElementById("ataque4");
+let chimuelo = {
+    name: 'chimuelo',
+    health: 100,
+    strength: 10,
+    defense: 9,
+    speed: 6,
+  };
+  
+  let amarillo = {
+    name: 'amarillo',
+    health: 100,
+    strength: 8,
+    defense: 7,
+    speed: 7,
+  };
+  function ataquechimuelo (){
+   
+  }
+  whoGoFirst(chimuelo, amarillo);
+  document.querySelector('ataque1').addEventListener('click', ataquechimuelo () {
+    chimueloTurn(chimuelo, amarillo);
+  });
