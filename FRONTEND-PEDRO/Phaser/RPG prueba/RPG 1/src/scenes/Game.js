365f9cb2408d06e47c2943c5b22ac38e93e285d8
Pedro Gutman <you@example.com>
2025-09-30 15:56:21 -0300

    Mapa grande con seguimiento de camara

--- FRONTEND-PEDRO/Phaser/RPG prueba/RPG 1/src/scenes/Game.js
+++ FRONTEND-PEDRO/Phaser/RPG prueba/RPG 1/src/scenes/Game.js
@@ -8,32 +8,37 @@ export class Game extends Phaser.Scene {
     preload() {
         //Carga del mapa
         this.load.image("tiles", "assets/town_forest_tiles.png");
-        this.load.tilemapTiledJSON("map", "assets/mapa_embebido.json");
+        this.load.tilemapTiledJSON("map", "assets/mapa2.json");
 
         //asignacion del sprite al personaje
          this.load.spritesheet('dude', 'assets/dude.png', {frameWidth: 32, frameHeight: 48});
     }
     create() {
         const map = this.make.tilemap({key: "map"});
-        const tileset = map.addTilesetImage("Woods", "tiles");
+        const tileset = map.addTilesetImage("mapa 2", "tiles");
         //creacion de las capas
-        const ground = map.createLayer("suelo", tileset, 0, 0);
+        const ground = map.createLayer("piso", tileset, 0, 0);
         const camino = map.createLayer("camino", tileset, 0, 0);
-        const obstaculos = map.createLayer("coliders", tileset, 0, 0);
-        const decorado = map.createLayer("Decorado", tileset, 0, 0);
+        const obstaculos = map.createLayer("obstaculos", tileset, 0, 0);
+       // const decorado = map.createLayer("Decorado", tileset, 0, 0);
         
         //jugador
      
-        this.player = new Player (this, 0, 96);
+        this.player = new Player (this, 16, 160);
         //colliders
-        this.player.setCollideWorldBounds(true);
+        ///this.player.setCollideWorldBounds(true);
 
         obstaculos.setCollisionByExclusion([-1]);
         this.physics.add.collider(this.player, obstaculos);
-        decorado.setCollisionByExclusion([-1]);
-        this.physics.add.collider(this.player, decorado);
-       //movimiento
+            //decorado.setCollisionByExclusion([-1]);
+            //this.physics.add.collider(this.player, decorado);
+      
+            //movimiento
        this.cursors = this.input.keyboard.createCursorKeys();
+       
+       //camara
+       this.cameras.main.startFollow(this.player);
+       this.cameras.main.setBounds(93, 49, map.widthInPixels, map.heightInPixels);
        
     }
 
---
+++ FRONTEND-PEDRO/Tiled/mapa 2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="mapa 2" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="90" columns="9">
+ <image source="town_forest_tiles.png" width="320" height="352"/>
+</tileset>
