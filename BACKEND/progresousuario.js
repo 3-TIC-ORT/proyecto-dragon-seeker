7c3190a9bd0f5ba8dd8f03c9821d4680f37fb512
FRANKITOBMW <133927017+FRANKITOBMW@users.noreply.github.com>
2025-10-17 12:11:49 -0300

    hay que seguir con progresousuario

--- BACKEND/progresousuario.js
+++ BACKEND/progresousuario.js
@@ -1,3 +1,39 @@
+import fs from "fs"
+
+const ruta = "./BACKEND/progreso.json"
+
+function leer() {
+    if (fs.writeFileSync(ruta,"[]"))
+    return JSON.parse(fs.readFileSync(ruta) || "[]")
+}
+
+function guardar(datos) {
+    fs.writeFileSync(ruta, JSON.stringify(datos,null,2))
+}
+
+function buscar(lista, user, dragon) {
+    return lista.find(x => x.user === user && x.dragon === dragon)
+}
+
+function asegurar(lista, user, dragon) {
+    let aaa =  buscar(lista, user, dragon)
+    if (!aaa) {
+        reg = {
+            user,
+            dragon,
+            nivel: 1,
+            exp: 0, 
+            vida: 100,
+            daño: 10,
+            ataques: [
+                {nombre: "arañazo", nivel: 1},
+                {nombre: "Llamarada", nivel: 5},
+                {nombre: "Terremoto", nivel: 13}
+            ]
+        }
+    }
+}
+
 export function sumarexperiencia(iddragon, cantidad, idusuario) {
 
 }
