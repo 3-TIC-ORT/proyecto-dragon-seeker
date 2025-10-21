7d3454b7590c4fbca130e269a2a0a7518ec0fd17
Lucas Waisbaum <you@example.com>
2025-10-21 14:34:17 -0300

    aaalgo de el inisio de sesion

--- inicioSesion/login.js
+++ inicioSesion/login.js
@@ -1,2 +1,5 @@
 let formulario = document.getElementById('formLogin');
 let mensaje = document.getElementById('mensaje');
+let contraseña = document.getElementById ("contraseña")
+let correo = document.getElementById ("usuario")
+postEvent("login", {correo,contraseña }, (formulario) => {
