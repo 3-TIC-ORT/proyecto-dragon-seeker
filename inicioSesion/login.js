3fcf9b80ced7c32f0338e0ccf3f74fc158007242
Lucas Waisbaum <you@example.com>
2025-10-22 10:44:55 -0300

    masomenos va tomando color

--- inicioSesion/login.js
+++ inicioSesion/login.js
@@ -1,5 +1,12 @@
+
 let formulario = document.getElementById('formLogin');
 let mensaje = document.getElementById('mensaje');
 let contraseña = document.getElementById ("contraseña")
 let correo = document.getElementById ("usuario")
-postEvent("login", {correo,contraseña }, (formulario) => {
+postEvent("login", { correo, contraseña }, (idUsuario) => {
+    if (idUsuario.exito === false) {
+        console.log(idUsuario.mensaje);
+    }
+    else if (idUsuario.exito === true) {
+        console.log(idUsuario.mensaje);
+}});
