7f519b03b215689237cd8072dcc069151e55286f
Pedro Gutman <you@example.com>
2025-10-29 12:10:33 -0300

    Falta arreglar los limites del dragon

--- FRONTEND-PEDRO/Phaser/RPG prueba/RPG 1/src/GameObjects/npc.js
+++ FRONTEND-PEDRO/Phaser/RPG prueba/RPG 1/src/GameObjects/npc.js
@@ -6,16 +6,21 @@ export class NPC extends Phaser.Physics.Arcade.Sprite{
         scene.physics.add.existing(this);
     
         this.speed = 50;
+
+        this.minX = 100;
+        this.maxX = 300;
+        this.minY = 200;
+        this.maxY = 400;
     
         // Definimos la secuencia de movimiento
         this.patrolSteps = [
-          { dx: 1, dy: 0, time: 2000 },  // derecha
+          { dx: 1, dy: 0, time: 4000 },  // derecha
           { dx: 0, dy: 0, time: 1000 },  // pausa
-          { dx: 0, dy: -1, time: 2000 }, // arriba
+          { dx: -1, dy: -1, time: 2000 }, // arriba
           { dx: 0, dy: 0, time: 1000 },  // pausa
-          { dx: -1, dy: 0, time: 2000 }, // izquierda
+          { dx: 1, dy: 0, time: 3000 }, // izquierda
           { dx: 0, dy: 0, time: 1000 },  // pausa
-          { dx: 0, dy: 1, time: 2000 },  // abajo
+          { dx: 0, dy: 1, time: 4000 },  // abajo
           { dx: 0, dy: 0, time: 1000 }   // pausa
         ];
     
@@ -29,7 +34,7 @@ export class NPC extends Phaser.Physics.Arcade.Sprite{
           
           this.anims.create({
             key: 'npc-left',
-            frames: this.anims.generateFrameNumbers('dragon', { start: 0, end: 4 }),
+            frames: this.anims.generateFrameNumbers('dragon', { start: 0, end: 3 }),
             frameRate: 8,
             repeat: -1
           });
@@ -48,14 +53,15 @@ export class NPC extends Phaser.Physics.Arcade.Sprite{
         if (step.dx === 0 && step.dy === 0) {
           // Pausa → quieto
           this.anims.stop();
-        } else if (step.dx > 0) {
+        } else if (step.dx > this.minX) {
+          this.setFlipX(true);
             this.anims.play('npc-left', true);
-            this.setFlipX(true);  
-        } else if (step.dx < 0) {
-          this.anims.play('npc-left', true);
-        } else if (step.dy > 0) {
+          } else if (step.dx < this.maxX) {  
+            this.setFlipX(false);
+            this.anims.play('npc-left', true);
+        } else if (step.dy > this.minY) {
           
-        } else if (step.dy < 0) {
+        } else if (step.dy < this.maxX) {
           
         }
     
