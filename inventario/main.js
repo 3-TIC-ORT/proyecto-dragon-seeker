29464a9278fa07b3695b1bf4c7f128f4fc8d119d
Lucas Waisbaum <you@example.com>
2025-10-29 10:41:21 -0300

    arrancando el inventario y complejizando la pelea

---
+++ inventario/main.js
@@ -0,0 +1,30 @@
+/*
+    Cargar comidas en memoria desde el JSON
+*/
+fetch('./BACKEND/dragones.json')          // Ruta al archivo JSON
+  .then(response => response.json())  // Convertir la respuesta en JSON
+  .then(data => {                     // Aquí tienes acceso al JSON en formato de objeto JS
+    console.log('dragones cargadas desde JSON:');
+    console.log(data);
+    dragones = data;                   // Asignar el JSON a la variable comidas
+    mostrarDragones()
+  }).catch(error => {                   // Manejo de errores al leer el archivo JSON
+    console.error('Error al leer el archivo JSON:', error);
+  });
+
+let dragones = [];
+const container = document.getElementById('dragonContainer');
+
+function mostrarDragones(){
+  dragones.forEach(dragon=> {
+    container.innerHTML += `
+      <div class = "tarjeta">
+      <h4>${dragon.nombre}</h4>
+      <p>${dragon.tipo}</p>
+      <p>${dragon.nivel}</p>
+      <p>${dragon.vida}</p>
+      <p>${dragon.fuerza}</p>
+      <p>${dragon.especial}</p>
+      <p>${dragon.mapa}</p>
+      </div>
+    `})}
