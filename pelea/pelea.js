645edb863b6393835827bbfc81ade1988fadb470
Lucas Waisbaum <you@example.com>
2025-11-26 15:54:06 -0300

    ya hice el css de los botones de pelear y adoptar

--- pelea/pelea.js
+++ pelea/pelea.js
@@ -3,7 +3,7 @@ let BotonAdopcion = document.getElementById("adoptar");
 let MenuAtaques = document.getElementById("menudeataques");
 function desplegarMenu(){
     MenuAtaques.style.transform = "translateX(0%)";
-    BotonPelea.style.transform = "translateX(-200%)";
+    BotonPelea.style.transform = "translateX(-400%)";
 }
 BotonPelea.addEventListener("click", desplegarMenu);
 export function desbloquearBoton() {
